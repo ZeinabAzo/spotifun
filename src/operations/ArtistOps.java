64c60ce388620dd76eb3f15e44b072041b7206e8
Zeinab Azordeh <z.azordeh2006@gmail.com>
2025-11-29 16:32:53 +0330

    Add artist related operations

--- src/operations/ArtistOps.java
+++ src/operations/ArtistOps.java
@@ -1,4 +1,29 @@
 package operations;
 
 public class ArtistOps {
+    SparseSet<Artist> artists;
+
+    public ArtistOps(){
+        artists = new SparseSet<>(100, 100); //based on my project document.
+    }
+
+    public void add(Artist artist) throws Exception {
+        artists.add(artist);
+    }
+
+    public void delete(int id) throws Exception {
+        artists.remove(id);
+    }
+
+    public Artist find(int id){
+        return artists.get(id);
+    }
+
+    public void printAll(){
+        System.out.println(Arrays.toString(artists.all()));
+    }
+
+    public void clear(){
+        artists = new SparseSet<>(100, 100);
+    }
 }
