7a1713e188fcc2bdb583d370a804be73757b1425
Zeinab Azordeh <z.azordeh2006@gmail.com>
2025-12-01 21:36:04 +0330

    Create main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
     public static void main(String[] args){
-
+        Scanner scanner = new Scanner(System.in);
+        CommandHandler commandHandler = new CommandHandler(scanner);
+        scanner.close();
     }
 }
