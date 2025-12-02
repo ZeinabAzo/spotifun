60c34e1cb216855e283fe0004058a2bc094a03f7
Zeinab Azordeh <z.azordeh2006@gmail.com>
2025-12-02 21:09:57 +0330

    Debug main so Scanner is safely closed

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import handlers.CommandHandler;
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        CommandHandler commandHandler = new CommandHandler(scanner);
-        commandHandler.start();
-        scanner.close();
+        try (Scanner scanner = new Scanner(System.in)) {
+            CommandHandler commandHandler = new CommandHandler(scanner);
+            commandHandler.start();
+        }
+        System.out.println("Exiting command prompt. Bye!");
     }
 }
+
--- src/handlers/ArtistCommand.java
+++ src/handlers/ArtistCommand.java
@@ -9,6 +9,7 @@ public class ArtistCommand {
     public void add(String[] args) {
         String name = args[0];
         int id = Integer.parseInt(args[1]);
+        System.out.println("--"+ name +" - "+ id);
         Artist artist = new Artist(name, id);
         artistOps.add(artist);
     }
